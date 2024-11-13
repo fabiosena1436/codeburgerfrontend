518e9bc4f4f2e8f829c8040bf65d40cef9d42979
fabiosena1436 <128174066+fabiosena1436@users.noreply.github.com>
2024-11-13 18:08:45 -0300

    ATT

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiCodeburger = axios.create({
-  // baseURL: "https://codeburger-backend-production-6629.up.railway.app/",
+  
   baseURL: "http://localhost:3001",
 })
 
